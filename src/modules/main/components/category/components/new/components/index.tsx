85199d04b9e8147633c41050b6ad0deef7d2f5eb
Tuan <103013479+TuanTien0401@users.noreply.github.com>
2022-04-29 14:07:57 +0700

    Co-authored-by: Libeyondea <libeyondea@users.noreply.github.com>

--- src/modules/main/components/category/components/new/components/index.tsx
+++ src/modules/main/components/category/components/new/components/index.tsx
@@ -4,29 +4,21 @@ import CardComponent from 'components/Card/components';
 import { FormikProps, useFormik } from 'formik';
 import { useNavigate } from 'react-router-dom';
 import * as routeConstant from 'constants/route';
-import * as postConstant from 'constants/post';
 import * as Yup from 'yup';
 import classNames from 'classnames';
-import ImageInput from 'components/ImageInput/components';
-import imageService from 'services/imageService';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { Fragment, useEffect, useState } from 'react';
 import toastify from 'helpers/toastify';
-import { CreatePostFormik } from 'models/post';
+import { CreateCategoryFormik } from 'models/category';
 import categoryService from 'services/categoryService';
 import { Category } from 'models/category';
-import CreatableSelect from 'react-select/creatable';
-import postService from 'services/postService';
 import _ from 'lodash';
-import JoditEditor from 'jodit-react';
-import { RichTextEditor } from '@mantine/rte';
+import { number } from 'yup/lib/locale';
 
 type Props = {};
 
-const NewPostComponent: React.FC<Props> = () => {
+const NewCategoryComponent: React.FC<Props> = () => {
 	const navigate = useNavigate();
-	const [isUploading, setUploading] = useState(false);
-	const [isCreating, setCreating] = useState(false);
 
 	const [state, setState] = useState<{
 		data: {
@@ -36,10 +28,7 @@ const NewPostComponent: React.FC<Props> = () => {
 			categories: boolean;
 		};
 		creating: {
-			post: boolean;
-		};
-		uploading: {
-			post: boolean;
+			category: boolean;
 		};
 	}>({
 		data: {
@@ -49,10 +38,7 @@ const NewPostComponent: React.FC<Props> = () => {
 			categories: true
 		},
 		creating: {
-			post: false
-		},
-		uploading: {
-			post: false
+			category: false
 		}
 	});
 
@@ -87,101 +73,36 @@ const NewPostComponent: React.FC<Props> = () => {
 			});
 	}, []);
 
-	const formik: FormikProps<CreatePostFormik> = useFormik<CreatePostFormik>({
+	const formik: FormikProps<CreateCategoryFormik> = useFormik<CreateCategoryFormik>({
 		enableReinitialize: true,
 		initialValues: {
-			title: '',
+			name: '',
 			slug: '',
-			excerpt: '',
-			content: '',
-			status: postConstant.POST_STATUS_DRAFT,
-			category_id: 0,
-			tags: [],
-			image: null
+			parent_id: ''
 		},
 		validationSchema: Yup.object({
-			title: Yup.string().required('The title is required.').max(255, 'The title must not be greater than 255 characters.'),
+			name: Yup.string().required('The name is required.').max(255, 'The name must not be greater than 255 characters.'),
 			slug: Yup.string().max(255, 'The slug must not be greater than 255 characters.').nullable(),
-			excerpt: Yup.string()
-				.required('The excerpt is required.')
-				.max(666, 'The excerpt must not be greater than 666 characters.'),
-			content: Yup.string()
-				.required('The content is required.')
-				.max(60000, 'The content must not be greater than 60000 characters.'),
-			category_id: Yup.number().positive('The category must be select').required('The category is required.'),
-			tags: Yup.array()
-				.required('The tags is required.')
-				.min(1, 'The tags must not be less than 1.')
-				.max(66, 'The tags must not be greater than 20.')
-				.of(
-					Yup.object().shape({
-						name: Yup.string().required().max(66, 'The tag name must not be greater than 66 characters.'),
-						slug: Yup.string().required().max(66, 'The tag slug must not be greater than 66 characters.')
-					})
-				),
-			status: Yup.string()
-				.required('The status is required.')
-				.oneOf(
-					[
-						postConstant.POST_STATUS_DRAFT,
-						postConstant.POST_STATUS_PENDING,
-						postConstant.POST_STATUS_PUBLISH,
-						postConstant.POST_STATUS_TRASH
-					],
-					'The status invalid.'
-				)
+			parent_id: Yup.string()
 		}),
 		onSubmit: (values, { setErrors }) => {
-			console.log(values);
-			new Promise<{ image?: string }>((resolve, reject) => {
-				if (!values.image) {
-					return resolve({});
+			setState((prevState) => ({
+				...prevState,
+				creating: {
+					...prevState.creating,
+					category: true
 				}
-				setUploading(true);
-				imageService
-					.upload({
-						image: values.image
-					})
-					.then((response) => {
-						return resolve({ image: response.data.data.image });
-					})
-					.catch((error) => {
-						return reject(error);
-					})
-					.finally(() => {
-						setUploading(false);
-					});
-			})
-				.then((result) => {
-					setCreating(true);
-					const payload = {
-						title: values.title,
-						slug: values.slug,
-						excerpt: values.excerpt,
-						content: values.content,
-						category_id: values.category_id,
-						tags: values.tags,
-						status: values.status,
-						...(result.image && {
-							image: result.image
-						})
-					};
-					postService
-						.create(payload)
-						.then((response) => {
-							toastify.success('Create user success');
-							navigate(`/${routeConstant.ROUTE_NAME_MAIN}/${routeConstant.ROUTE_NAME_MAIN_POST}`);
-						})
-						.catch((error) => {
-							if (axios.isAxiosError(error)) {
-								if (error.response?.data.errors && error.response.status === 400) {
-									setErrors(error.response.data.errors);
-								}
-							}
-						})
-						.finally(() => {
-							setCreating(false);
-						});
+			}));
+			const payload = {
+				name: values.name,
+				slug: values.slug,
+				parent_id: values.parent_id
+			};
+			categoryService
+				.create(payload)
+				.then((response) => {
+					toastify.success('Create category success');
+					navigate(`/${routeConstant.ROUTE_NAME_MAIN}/${routeConstant.ROUTE_NAME_MAIN_CATEGORY}`);
 				})
 				.catch((error) => {
 					if (axios.isAxiosError(error)) {
@@ -190,21 +111,25 @@ const NewPostComponent: React.FC<Props> = () => {
 						}
 					}
 				})
-				.finally(() => {});
+				.finally(() => {
+					setState((prevState) => ({
+						...prevState,
+						creating: {
+							...prevState.creating,
+							category: false
+						}
+					}));
+				});
 		}
 	});
 
-	const handleImageUpload = (image: File): Promise<string> =>
-		new Promise<string>((resolve, reject) => {
-			imageService
-				.upload({
-					image: image
-				})
-				.then((response) => {
-					return resolve(response.data.data.image);
-				})
-				.catch(() => reject(new Error('Upload failed')));
-		});
+	const levelCategories = (level: number) => {
+		let test = [];
+		for (let i = 0; i < level; i++) {
+			test.push(<BsDashLg className="inline-block mr-1 text-gray-400" />);
+		}
+		return test;
+	};
 
 	const recursiveCategories = (categories: Category[]) => {
 		return categories.map((category) => (
@@ -217,53 +142,35 @@ const NewPostComponent: React.FC<Props> = () => {
 
 	return (
 		<>
-			<BreadcrumbComponent className="mb-4">New user</BreadcrumbComponent>
+			<BreadcrumbComponent className="mb-4">New category</BreadcrumbComponent>
 			<div className="grid grid-cols-1 gap-4">
 				<div className="col-span-1 w-full">
-					<CardComponent header="New user">
+					<CardComponent header="New category">
 						<form onSubmit={formik.handleSubmit}>
 							<div className="grid grid-cols-2 gap-4">
-								<div className="col-span-2">
-									<label htmlFor="image" className="inline-block font-medium text-gray-600 mb-1">
-										Image
-									</label>
-									<div className="relative">
-										<ImageInput
-											name="image"
-											id="image"
-											onChangeCustom={formik.setFieldValue}
-											onBlurCustom={formik.setFieldTouched}
-											isLarge
-										/>
-									</div>
-									{formik.errors.image && formik.touched.image && (
-										<div className="text-red-700 mt-1 text-sm">{formik.errors.image}</div>
-									)}
-								</div>
 								<div className="col-span-2 md:col-span-1">
-									<label htmlFor="title" className="inline-block font-medium text-gray-600 mb-1">
-										Title
+									<label htmlFor="name" className="inline-block font-medium text-gray-600 mb-1">
+										Name
 									</label>
 									<div className="relative">
 										<input
 											type="text"
-											placeholder="Enter title"
+											placeholder="Enter name"
 											className={classNames(
 												'rounded-md flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent',
 												{
-													'focus:ring-red-600 border-red-600':
-														formik.errors.title && formik.touched.title
+													'focus:ring-red-600 border-red-600': formik.errors.name && formik.touched.name
 												}
 											)}
 											onChange={formik.handleChange}
 											onBlur={formik.handleBlur}
-											value={formik.values.title}
-											name="title"
-											id="title"
+											value={formik.values.name}
+											name="name"
+											id="name"
 										/>
 									</div>
-									{formik.errors.title && formik.touched.title && (
-										<div className="text-red-700 mt-1 text-sm">{formik.errors.title}</div>
+									{formik.errors.name && formik.touched.name && (
+										<div className="text-red-700 mt-1 text-sm">{formik.errors.name}</div>
 									)}
 								</div>
 								<div className="col-span-2 md:col-span-1">
@@ -291,79 +198,8 @@ const NewPostComponent: React.FC<Props> = () => {
 										<div className="text-red-700 mt-1 text-sm">{formik.errors.slug}</div>
 									)}
 								</div>
-								<div className="col-span-2">
-									<label htmlFor="excerpt" className="inline-block font-medium text-gray-600 mb-1">
-										Excerpt
-									</label>
-									<div className="relative">
-										<textarea
-											rows={4}
-											placeholder="Enter excerpt"
-											className={classNames(
-												'rounded-md flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent',
-												{
-													'focus:ring-red-600 border-red-600':
-														formik.errors.excerpt && formik.touched.excerpt
-												}
-											)}
-											onChange={formik.handleChange}
-											onBlur={formik.handleBlur}
-											value={formik.values.excerpt}
-											name="excerpt"
-											id="excerpt"
-										></textarea>
-									</div>
-									{formik.errors.excerpt && formik.touched.excerpt && (
-										<div className="text-red-700 mt-1 text-sm">{formik.errors.excerpt}</div>
-									)}
-								</div>
-								<div className="col-span-2">
-									<label htmlFor="content" className="inline-block font-medium text-gray-600 mb-1">
-										Content
-									</label>
-									<div className="relative">
-										<JoditEditor
-											value={formik.values.content}
-											config={{
-												readonly: false,
-												minHeight: 666
-											}}
-											onBlur={(value) => formik.setFieldValue('content', value)}
-											onChange={(value) => {}}
-										/>
-										{/* <RichTextEditor
-											className="rich-text-editor"
-											placeholder="Enter content"
-											value={formik.values.content}
-											onChange={(value) => formik.setFieldValue('content', value)}
-											onBlur={() => formik.setFieldTouched('content', true)}
-											onImageUpload={handleImageUpload}
-											sticky={true}
-											stickyOffset={56}
-										/> */}
-										{/* <textarea
-											rows={8}
-											placeholder="Enter content"
-											className={classNames(
-												'rounded-md flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent',
-												{
-													'focus:ring-red-600 border-red-600':
-														formik.errors.content && formik.touched.content
-												}
-											)}
-											onChange={formik.handleChange}
-											onBlur={formik.handleBlur}
-											value={formik.values.content}
-											name="content"
-											id="content"
-										></textarea> */}
-									</div>
-									{formik.errors.content && formik.touched.content && (
-										<div className="text-red-700 mt-1 text-sm">{formik.errors.content}</div>
-									)}
-								</div>
 								<div className="col-span-2 md:col-span-1">
-									<label htmlFor="category_id" className="inline-block font-medium text-gray-600 mb-1">
+									<label htmlFor="parent_id" className="inline-block font-medium text-gray-600 mb-1">
 										Category
 									</label>
 									<div className="relative">
@@ -372,114 +208,45 @@ const NewPostComponent: React.FC<Props> = () => {
 												'capitalize rounded-md flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent',
 												{
 													'focus:ring-red-600 border-red-600':
-														formik.errors.category_id && formik.touched.category_id
+														formik.errors.parent_id && formik.touched.parent_id
 												}
 											)}
 											onChange={formik.handleChange}
 											onBlur={formik.handleBlur}
-											value={formik.values.category_id}
-											name="category_id"
-											id="category_id"
+											value={formik.values.parent_id}
+											name="parent_id"
+											id="parent_id"
 											disabled={state.loading.categories || !state.data.categories.length}
 										>
 											{state.loading.categories ? (
-												<option value={0}>Loading...</option>
+												<option value={''}>Loading...</option>
 											) : !state.data.categories.length ? (
-												<option value={0}>Empty</option>
+												<option value={''}>Empty</option>
 											) : (
 												<Fragment>
-													<option value={0}>---Select---</option>
+													<option value={''}>---Select---</option>
 													{recursiveCategories(state.data.categories)}
 												</Fragment>
 											)}
 										</select>
 									</div>
-									{formik.errors.category_id && formik.touched.category_id && (
-										<div className="text-red-700 mt-1 text-sm">{formik.errors.category_id}</div>
+									{formik.errors.parent_id && formik.touched.parent_id && (
+										<div className="text-red-700 mt-1 text-sm">{formik.errors.parent_id}</div>
 									)}
 								</div>
-								<div className="col-span-2 md:col-span-1">
-									<label htmlFor="tags" className="inline-block font-medium text-gray-600 mb-1">
-										Tags
-									</label>
-									<div className="relative">
-										<CreatableSelect
-											id="tags"
-											name="tags"
-											isClearable
-											isMulti
-											placeholder="Choose tags"
-											onChange={(value) => formik.setFieldValue('tags', value)}
-											onBlur={() => formik.setFieldTouched('tags', true)}
-											value={formik.values.tags}
-											getNewOptionData={(inputValue, optionLabel) => ({
-												name: inputValue,
-												slug: inputValue
-											})}
-											getOptionLabel={(option) => option.name}
-											getOptionValue={(option) => option.slug}
-										/>
-									</div>
-									{formik.errors.tags && formik.touched.tags && (
-										<div className="text-red-700 mt-1 text-sm">
-											{_.isString(formik.errors.tags) && formik.errors.tags}
-											{_.isArray(formik.errors.tags) &&
-												formik.errors.tags.map((tag) => (_.isString(tag) ? tag : tag?.name))}
-										</div>
-									)}
-								</div>
-								<div className="col-span-2 md:col-span-1">
-									<label htmlFor="status" className="inline-block font-medium text-gray-600 mb-1">
-										Status
-									</label>
-									<div className="relative">
-										<select
-											className={classNames(
-												'capitalize rounded-md flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent',
-												{
-													'focus:ring-red-600 border-red-600':
-														formik.errors.status && formik.touched.status
-												}
-											)}
-											onChange={formik.handleChange}
-											onBlur={formik.handleBlur}
-											value={formik.values.status}
-											name="status"
-											id="status"
-										>
-											{[
-												postConstant.POST_STATUS_DRAFT,
-												postConstant.POST_STATUS_PENDING,
-												postConstant.POST_STATUS_PUBLISH,
-												postConstant.POST_STATUS_TRASH
-											].map((status, index) => (
-												<option value={status} key={index}>
-													{status}
-												</option>
-											))}
-										</select>
-									</div>
-									{formik.errors.status && formik.touched.status && (
-										<div className="text-red-700 mt-1 text-sm">{formik.errors.status}</div>
-									)}
-								</div>
+
 								<div className="col-span-2">
 									<button
 										type="submit"
 										className={classNames(
 											'flex items-center justify-center py-3 px-4 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white transition ease-in duration-200 text-sm font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-md',
 											{
-												'cursor-not-allowed disabled:opacity-50': isUploading || isCreating
+												'cursor-not-allowed disabled:opacity-50': state.creating.category
 											}
 										)}
-										disabled={isUploading || isCreating}
+										disabled={state.creating.category}
 									>
-										{isUploading ? (
-											<>
-												<AiOutlineLoading3Quarters className="animate-spin h-4 w-4 mr-2 font-medium" />
-												<span>Uploading</span>
-											</>
-										) : isCreating ? (
+										{state.creating.category ? (
 											<>
 												<AiOutlineLoading3Quarters className="animate-spin h-4 w-4 mr-2 font-medium" />
 												<span>Creating</span>
@@ -498,4 +265,4 @@ const NewPostComponent: React.FC<Props> = () => {
 	);
 };
 
-export default NewPostComponent;
+export default NewCategoryComponent;
